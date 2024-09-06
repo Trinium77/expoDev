6e5a3a2cc8b1a7ed8c4d8aa6bf1111cc9d06954d
Mohamed Zeba <parice02@hotmail.com>
2024-09-06 16:31:31 +0000

    push

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
-import { Text, View, StyleSheet, Image, Button } from "react-native";
+import { Text, View, StyleSheet, TextInput, FlatList } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { movies } from "@/scripts/movies";
 
 const styles = StyleSheet.create({
   view1: { flex: 1, backgroundColor: "red", justifyContent: "center", alignItems: "center" },
@@ -7,19 +10,31 @@ const styles = StyleSheet.create({
   view3: { flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center" },
   view4: { flex: 1, backgroundColor: "blue" },
   row: {
-    flex: 1,
     flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 10,
   },
   text: { color: "black", fontSize: 30 },
 });
 
 export default function Index() {
-  const [view3, setView3] = useState(0);
+  const [text, setText] = useState("");
+  const [display, setDisplay] = useState("");
 
   const view4 = "Texte de la View 4";
 
   const onPressButton = () => {
-    setView3((state) => state + 1);
+    setDisplay(`Vous avez saisi: ${text}`);
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <>
+        <Text>Titre: {item.title}</Text>
+        <Text>Résumé: {item.overview}</Text>
+      </>
+    );
   };
 
   return (
@@ -28,27 +43,20 @@ export default function Index() {
         flex: 1,
       }}>
       <View style={styles.row}>
-        <View style={styles.view1}>
-          <Image
-            source={require("@/assets/images/react-logo.png")}
-            alt="logo react-native"
-            resizeMethod={"auto"}
-            resizeMode={"center"}
-          />
-        </View>
-        <View style={styles.view2}>
-          <Button title="cliquer" color={"pink"} onPress={onPressButton} />
-        </View>
+        <TextInput
+          autoFocus={true}
+          style={{ flex: 1, borderWidth: 2, padding: 5, marginRight: 15 }}
+          onChangeText={setText}
+          value={text}
+        />
+        <Ionicons name={"search"} size={30} onPress={onPressButton} />
       </View>
       <View
         style={{
           flex: 5,
         }}>
         <View style={styles.view3}>
-          <Text style={styles.text}>{view3}</Text>
-        </View>
-        <View style={styles.view4}>
-          <Text style={styles.text}>{view4}</Text>
+          <Text style={styles.text}>{display}</Text>
         </View>
       </View>
     </View>
