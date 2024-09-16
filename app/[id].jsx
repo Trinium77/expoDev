ca815bc7c9f32285ba65bc2931fb4d7e2328e758
Mohamed Zeba <parice02@hotmail.com>
2024-09-16 17:00:47 +0000

    change FlatList to VirtualizedList

--- app/[id].jsx
+++ app/[id].jsx
@@ -1,22 +1,60 @@
-import { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Text, Image, StyleSheet, ActivityIndicator } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 
+import { movies } from "@/scripts/movies";
+
 export default function Detail() {
-  const local = useLocalSearchParams();
+  const { id } = useLocalSearchParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const selectedMovie = movies.results.find((movie) => movie.id === parseInt(id));
+    setMovie(selectedMovie);
+  }, [id]);
+
+  if (!movie) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "white",
-      }}>
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-        <Text>Ceci est la page de détail {local.id}</Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>{movie.title}</Text>
+      <Image
+        source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+        style={styles.poster}
+      />
+      <Text style={styles.description}>{movie.overview}</Text>
+      <Text style={styles.details}>Release Date: {movie.release_date}</Text>
+      <Text style={styles.details}>Vote Average: {movie.vote_average}</Text>
+      <Text style={styles.details}>Vote Count: {movie.vote_count}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "white",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+  poster: {
+    width: "auto",
+    height: 450,
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  details: {
+    fontSize: 14,
+    color: "gray",
+  },
+});
--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { View, StyleSheet, FlatList, Pressable } from "react-native";
+import { useState, useEffect } from "react";
+import { View, StyleSheet, VirtualizedList, Pressable } from "react-native";
 import { Link } from "expo-router";
 
 import ListItem from "@/components/ListItem";
@@ -7,10 +7,38 @@ import EmptyList from "@/components/EmptyList";
 import HeaderFooter from "@/components/HeaderFooter";
 import Searchbox from "@/components/Searchbox";
 
-import { movies } from "@/scripts/movies";
+//import { movies } from "@/scripts/movies";
+import { token, url } from "@/scripts/tools";
+
+const config = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+};
 
 export default function Index() {
   const [text, setText] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [movieList, setMovieList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getMovies = async (page) => {
+    try {
+      const response = await fetch(url + `&page=${page}`, config);
+      return await response.json();
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    (async () => {
+      const m = await getMovies(page);
+      setMovieList(m.results);
+      setLoading(false);
+    })();
+  }, []);
 
   const onPressButton = () => {};
 
@@ -21,6 +49,20 @@ export default function Index() {
       </Pressable>
     </Link>
   );
+
+  const getItem = (_data, index) => _data.at(index);
+
+  const getItemCount = (_data) => _data.length;
+
+  const onEndReached = async () => {
+    setLoading(true);
+    const m = await getMovies(page + 1);
+    setMovieList((oldMovies) => [...oldMovies, ...m.results]);
+    setLoading(false);
+    setPage((page) => page + 1);
+  };
+
+  const onRefresh = async () => {};
 
   const renderSeparator = () => <View style={{ marginVertical: 3 }} />;
 
@@ -35,13 +77,19 @@ export default function Index() {
         style={{
           flex: 5,
         }}>
-        <FlatList
-          data={movies.results}
+        <VirtualizedList
+          data={movieList}
           renderItem={renderItem}
           ItemSeparatorComponent={renderSeparator}
           ListFooterComponent={HeaderFooter}
           ListHeaderComponent={HeaderFooter}
           ListEmptyComponent={EmptyList}
+          keyExtractor={(item, index) => index}
+          refreshing={loading}
+          onEndReached={onEndReached}
+          getItem={getItem}
+          getItemCount={getItemCount}
+          onRefresh={onRefresh}
         />
       </View>
     </View>
