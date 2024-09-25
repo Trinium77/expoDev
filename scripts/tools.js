8e0a12895537b5ec0260f44b53208b904c85438e
Mohamed Zeba <parice02@hotmail.com>
2024-09-25 11:53:48 +0000

    df

--- scripts/tools.js
+++
@@ -1,7 +0,0 @@
-const token = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjgwNGNkZTliYTY0Y2NlOTEzY2JkMTcyODA1MzdjZSIsIm5iZiI6MTcyNTYzOTIyMC45OTEwODEsInN1YiI6IjVlMTBmNmJlYWUyNmJlMDAxMjQ4MWY3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WW7jDX4CiGuma_bxY1XMIaTunJnflzfScyfJe4H7s2E`;
-
-const url = "https://api.themoviedb.org/3/movie/popular?language=fr-FR";
-
-const detail_url = "https://api.themoviedb.org/3/movie/1022789?language=en-US";
-
-export { token, url };
